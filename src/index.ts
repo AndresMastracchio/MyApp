efe4dffb55fec149e10b13db4bbb959d6e25b73b
AndresMastracchio <104596541+AndresMastracchio@users.noreply.github.com>
2022-04-28 17:47:35 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello World!</h1>
+<div>
+  Esto es un HTML insertado desde TS, pero podríamos
+  moverlo al index! 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
