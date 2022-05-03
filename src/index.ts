c62e9a84dd0ec81515c44609b98ccd3a0645b971
AndresMastracchio <104596541+AndresMastracchio@users.noreply.github.com>
2022-05-02 21:12:13 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import "./styles.css";
+// Resolver el área de un cuadrado ingresando por pantalla en 2 inputs los valores
+// para mostrar el resultado por consola
 
-document.getElementById("app").innerHTML = `
-<h1>Hello World!</h1>
-<div>
-  Esto es un HTML insertado desde TS, pero podríamos
-  moverlo al index! 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+//rotulo1.innerHtml = "Ingresar la base: ";
+//rotulo2.innerHtml = "Ingresar la altura: ";
+
+let btnEnviar = document.getElementById("btnEnviar");
+let datoBase = document.getElementById("base");
+let datoAltura = document.getElementById("altura");
+
+btnEnviar.addEventListener("click", () => {
+  let area: number = Number(datoBase.value) * Number(datoAltura.value);
+  console.log("Resultado del área:", area);
+});
