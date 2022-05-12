6b483481fd55d247d1a5e3480676714c60292ed6
AndresMastracchio <104596541+AndresMastracchio@users.noreply.github.com>
2022-05-12 18:21:09 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import "./styles.css";
+rotulo1.innerHTML = "Ingrese un número: ";
+rotulo2.innerHTML = "Ingrese un número: ";
+rotulo3.innerHTML =
+  "Ingrese 1 para sumar, 2 para restar, cualquier otra tecla para salir: ";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello World!</h1>
-<div>
-  Esto es un HTML insertado desde TS, pero podríamos
-  moverlo al index! 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let btnEnviar = document.getElementById("btnEnviar");
+
+//let i: number, linea: string;
+//let numero1: number = Number(dato1.value);
+//let numero2: number = Number(dato2.value);
+//let opcionMenu: number = Number(dato3.value);
+
+btnEnviar.addEventListener("click", () => {
+  let i: number, linea: string;
+  let numero1: number = Number(dato1.value);
+  let numero2: number = Number(dato2.value);
+  let opcionMenu: number = Number(dato3.value);
+
+  if (opcionMenu === 1) {
+    linea = "-";
+    for (i = 0; i <= 40; i++) {
+      linea = linea + "-";
+    }
+    console.log(linea);
+    console.log("el resultado es: ", numero1 + numero2);
+    linea = "-";
+    for (i = 0; i <= 40; i++) {
+      linea = linea + "-";
+    }
+    console.log(linea);
+  } else if (opcionMenu === 2) {
+    linea = "-";
+    for (i = 0; i <= 40; i++) {
+      linea = linea + "-";
+    }
+    console.log(linea);
+    console.log("el resultado es: ", numero1 - numero2);
+    linea = "-";
+    for (i = 0; i <= 40; i++) {
+      linea = linea + "-";
+    }
+    console.log(linea);
+  }
+});
